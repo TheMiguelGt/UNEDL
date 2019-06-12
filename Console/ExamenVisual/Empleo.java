b25416f9387db4bc7e4c4e13fac7e4df89932db5
TheMiguelGt <48323585+TheMiguelGt@users.noreply.github.com>
2019-06-12 17:04:35 -0500

    Add files via upload
    Trabajos  
    -ordenamiento java
    -exception 
    -hora de entrada

---
+++ Console/ExamenVisual/Empleo.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial2;
+
+public class Empleo {
+    
+public static void main(String args[]){
+   
+}
+    
+    class popo{
+    private String nombre;
+    private int edad;
+
+    public popo(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    }
+}
